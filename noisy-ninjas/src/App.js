06071b077c23eb4871b5659592e81a2660458d6d
Andy Phy Lim <36771187+PhyLimChordata@users.noreply.github.com>
2022-07-13 01:03:41 -0700

    feat: implement ninja hexagon grid component

--- noisy-ninjas/src/App.js
+++ noisy-ninjas/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg'
 import './App.css'
 
+import { Hexagon } from './components/hexagon'
+import { NinjaHexagonGrid } from './components/hexagongrid-ninja'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NinjaHexagonGrid/>
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
