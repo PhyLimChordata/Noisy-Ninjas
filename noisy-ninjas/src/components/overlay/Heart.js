55b42bceab3c4e326cb906aae5b31dcab941feb2
Andy Phy Lim <36771187+PhyLimChordata@users.noreply.github.com>
2022-07-17 09:21:14 -0700

    feat: hearts

--- noisy-ninjas/src/components/overlay/Heart.js
+++ noisy-ninjas/src/components/overlay/Heart.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import '../../style/overlay.css'
 
 export function Hearts(props) {
-  
-  
   return <div id={props.id} className="hearts">
+    {[...Array(props.amount)].map(()=>(
+       <div className="live-heart">
+       </div>
+    ))}
   </div>
 }
--- noisy-ninjas/src/components/overlay/moveset.js
+++ noisy-ninjas/src/components/overlay/moveset.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../style/overlay.css'
 
-// import { Hearts } from './Heart'
+import { Hearts } from './Heart'
 
 export function Moveset(props) {
 
@@ -11,7 +11,7 @@ export function Moveset(props) {
     <div id="move3" className="move"><p>Item</p></div>
     <div id="move4" className="move"><p>Etc</p></div>
     <div id="timer">1:00</div>
-    {/* <Hearts id="hearts" amount={3}></Hearts> */}
+    <Hearts id="hearts" amount={3}></Hearts>
     <div id="hearts"></div>
   </div>
 }
