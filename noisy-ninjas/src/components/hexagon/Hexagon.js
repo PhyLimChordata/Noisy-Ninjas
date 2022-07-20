c941694cb7e9a23c7fcf7951c06dae988751f4bf
Calvin <calvincheng2000@gmail.com>
2022-07-20 15:44:04 -0400

    feat: players shows on board

--- noisy-ninjas/src/components/hexagon/Hexagon.js
+++ noisy-ninjas/src/components/hexagon/Hexagon.js
@@ -3,11 +3,22 @@ import '../../style/hexagon.css'
 
 export function Hexagon(props) {
   const {info, id, onClick, mode, hover, unhover} = props
-  const {color, x, y} = info
+  const {color, x, y, type, players} = info
+  function getClassName() {
+    if (players) {
+      if (players[0] == "ChoIsBest") {
+        return "ninja"
+      } else if (players[0] == "Cho'sMenace") {
+        return "scream"
+      }
+    } else {
+      return type[0] || ""
+    }
+  }
   return <div id={"hex" + id} className={mode === "move" ? "hexagon-move" : "hexagon"} onClick={() => onClick(x, y, id, info)} onMouseEnter={hover ? () => hover(id, 3) : ()=>{}} onMouseLeave={unhover ? ()=>unhover(id, 3) : ()=>{}}>
     <div className="background-hex">
       <div className="content-hex" style={{backgroundColor: color}}>
-        <div className={info.type ? info.type[0] : ""}></div>
+        <div className={getClassName()}></div>
       </div>
     </div>
   </div>
