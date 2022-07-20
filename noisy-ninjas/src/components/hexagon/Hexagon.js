f3517c8f8b225f37d2f9c53a0d1cf00ba8c5e86c
Calvin <calvincheng2000@gmail.com>
2022-07-20 16:02:32 -0400

    chore: showninja

--- noisy-ninjas/src/components/hexagon/Hexagon.js
+++ noisy-ninjas/src/components/hexagon/Hexagon.js
@@ -5,16 +5,17 @@ export function Hexagon(props) {
   const {info, id, onClick, mode, hover, unhover} = props
   const {color, x, y, type, players} = info
   function getClassName() {
-    if (players) {
-      if (players[0] == "ChoIsBest") {
-        return "ninja"
-      } else if (players[0] == "Cho'sMenace") {
-        return "monster"
-      }
-    } else {
-      return type[0] || ""
+    // if (players && players[0]) {
+    //   console.log(players)
+    // }
+    if (players && players[0] == "ChoIsBest") {
+      return "ninja"
+    } else if (players && players[0] == "Cho'sMenace") {
+      return "scream"
     }
+    return type && type[0] || ""
   }
+
   return <div id={"hex" + id} className={mode === "move" ? "hexagon-move" : "hexagon"} onClick={() => onClick(x, y, id, info)} onMouseEnter={hover ? () => hover(id, 3) : ()=>{}} onMouseLeave={unhover ? ()=>unhover(id, 3) : ()=>{}}>
     <div className="background-hex">
       <div className="content-hex" style={{backgroundColor: color}}>
