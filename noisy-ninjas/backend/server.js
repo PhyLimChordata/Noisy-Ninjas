18c9aa5c3d491cb5cd1135e15f484d8636d224d5
Andy Phy Lim <36771187+PhyLimChordata@users.noreply.github.com>
2022-07-27 22:41:25 -0700

    feat: win and lose rank endpoint

--- noisy-ninjas/backend/server.js
+++ noisy-ninjas/backend/server.js
@@ -169,7 +169,7 @@ app.patch("/api/users/:displayName/password",  function (req, res) {
 });
 
 app.patch("/api/users/:displayName/username",  function (req, res) {
-    const displayName = req.params.displayName
+    const displayName = req.params.displayName;
     const newName = req.body.username;
     if (!('username' in req.body)) return res.status(400).end('username is missing');
     
@@ -193,6 +193,49 @@ app.patch("/api/users/:displayName/username",  function (req, res) {
         
             });
 });
+
+app.patch("/api/users/:displayName/win", function (req, res) {
+    const displayName = req.params.displayName;
+    User.findOne({displayName}, function (err, user) {
+        if (err) return res.status(500).end(err);
+        let newPoints = user.points + 5;
+        let beltRank = "N/A";
+        if (newPoints <= 10) {
+            beltRank = "white";
+        } else if (newPoints <= 20) {
+            beltRank = "yellow";
+        } else if (newPoints <= 35) {
+            beltRank = "Green";
+        } else if (newPoints <= 75) {
+            beltRank = "Blue";
+        } else if (newPoints <= 120) {
+            beltRank = "Red";
+        } else {
+            beltRank = "Black";
+        }
+
+        User.findOneAndUpdate({displayName}, {points: newPoints, beltRank: beltRank}).exec(function(err) {
+            if (err) return res.status(500).end(err);
+            return res.json({promoted: beltRank !== user.beltRank, message: "+5 points have been added to " + displayName + "\'s account. The user now has " + (user.points + 5) + " points. The user is a " + beltRank + " belt."});
+        });
+    });
+});
+
+app.patch("/api/users/:displayName/lose", function (req, res) {
+    const displayName = req.params.displayName;
+    User.findOne({displayName}, function (err, user) {
+        if (err) return res.status(500).end(err);
+        let newPoints = 0;
+        if (user.points - 3 !== 0) {
+            newPoints = user.points - 3;
+        }
+        User.findOneAndUpdate({displayName}, {points: newPoints}).exec(function(err) {
+            if (err) return res.status(500).end(err);
+            return res.json("+5 points have been added to " + displayName + "\'s account. The user now has " + newPoints + " points");
+        });
+    });
+});
+
 
 app.get('/signout/', function(req, res, next){
     req.session.destroy();
@@ -254,7 +297,7 @@ app.get('/signout/', function(req, res, next){
               return res.status(409).end("displayName " + displayName + " already exists");
           bcrypt.hash(password, saltRounds, function (err, hash) {
               const ninja = "https://api.time.com/wp-content/uploads/2019/04/tyler-blevins-ninja-time-100-2019-002-1.jpg?quality=85&zoom=2"
-              const new_user = new User({googleID: "N/A",  displayName: req.body.displayName, imageURL: ninja, points: 0, hash: hash})
+              const new_user = new User({googleID: "N/A",  displayName: req.body.displayName, imageURL: ninja, points: 5, beltRank: "white", hash: hash})
               new_user.save(function(err){
                       if (err) return res.status(500).end(err);
                       return res.json(displayName);
