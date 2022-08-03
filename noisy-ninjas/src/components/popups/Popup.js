9984e4e2255b5f1d96a9ab0d52ff697a16faf560
Calvin <calvincheng2000@gmail.com>
2022-08-03 09:22:46 -0400

    chore: style css sheets

--- noisy-ninjas/src/components/popups/Popup.js
+++ noisy-ninjas/src/components/popups/Popup.js
@@ -4,7 +4,7 @@ export function Popup(props) {
   const { body, title, footer, header, modalStyle, backgroundStyle } = props
   return (
     <div className="modal-component" style={{ ...backgroundStyle }}>
-      <div className="modal_content" style={{ ...modalStyle }}>
+      <div className="modal-content" style={{ ...modalStyle }}>
         {header}
         <div className={'title'}>{title}</div>
         <div className={'body-content'}>{body}</div>
